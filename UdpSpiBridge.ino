1d15f2fca486c0e651fe5ac494f89a4dcc0bcc11
Klops666 <43826911+Klops666@users.noreply.github.com>
2019-07-24 11:10:04 +0200

    Add files via upload

---
+++ UdpSpiBridge.ino
@@ -0,0 +1,110 @@
+/*
+ UDPSendReceiveString:
+ This sketch receives UDP message strings, prints them to the serial port
+ and sends an "acknowledge" string back to the sender
+
+ A Processing sketch is included at the end of file that can be used to send
+ and received messages for testing with a computer.
+
+ created 21 Aug 2010
+ by Michael Margolis
+
+ This code is in the public domain.
+ */
+
+
+#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <Ethernet.h>
+#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+
+
+SPISettings settingsA(2000000, MSBFIRST, SPI_MODE0);
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+
+unsigned int localPort = 8888;      // local port to listen on
+
+// buffers for receiving and sending data
+char inputBuffer[UDP_TX_PACKET_MAX_SIZE];  //buffer to hold incoming packet,
+char outputBuffer[UDP_TX_PACKET_MAX_SIZE];  //buffer to hold incoming packet,
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  SPI.begin();
+  // this check is only needed on the Leonardo:
+  while (!Serial) {delay(10);} // wait for serial port to connect. Needed for native USB port only
+
+  Serial.println("Trying to get an IP address using DHCP");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    while(true){delay(500);}
+  }
+
+  Serial.print("My IP address: ");
+  IPAddress ip = Ethernet.localIP();
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(ip[thisByte], DEC);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Udp.begin(localPort);
+  Serial.print("Port: ");
+  Serial.println(localPort);
+
+  pinMode(SS,OUTPUT);
+}
+
+void loop() {
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i = 0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+
+    // read the packet into packetBufffer
+    Udp.read(inputBuffer, packetSize);
+    Serial.println("Input:Output");
+
+    digitalWrite(SS,LOW);
+    SPI.beginTransaction(settingsA);
+    for (int i = 0; i < packetSize; i++ )
+    {
+      outputBuffer[i] = SPI.transfer(inputBuffer[i]);
+      
+      Serial.print(String (inputBuffer[i],DEC));
+      Serial.print("\t");
+      Serial.println(String (outputBuffer[i],DEC));
+    }
+    SPI.endTransaction();
+    digitalWrite(SS,HIGH);
+    
+    
+    // send a reply to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(outputBuffer,packetSize);
+    Udp.endPacket();
+  }
+  delay(10);
+}
+
